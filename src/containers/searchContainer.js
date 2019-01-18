eab7f8de25da35d016b071b91a6917140486dcd4
AvenMerk <kremnevaaj@gmail.com>
2019-01-18 17:52:22 +0300

    Add radio buttons for arrive/departure and switch for delayed flights

--- src/containers/searchContainer.js
+++ src/containers/searchContainer.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Switch from '@material-ui/core/Switch';
+import Radio from '@material-ui/core/Radio';
 
-const SearchContainer = () => (
-    <Grid item xs={4}>
-      <div className="aero-search-container">
-          <h3>Searching parameters</h3>
-      </div>
-    </Grid>
-);
+class SearchContainer extends React.Component {
+    state = {
+        //для свитч
+        checkedA: true,
+        checkedB: true,
+        //для радио
+        selectedValue: 'arrive',
+    };
+
+    handleChangeSwitch = name => event => {
+        this.setState({ [name]: event.target.checked });
+    };
+
+    handleChangeRadio = event => {
+        this.setState({ selectedValue: event.target.value });
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <Grid item xs={4} className="aero-search-container">
+                    <div className="aero-search-container">
+                        <div>
+                            <h3>Параметры поиска</h3>
+                        </div>
+                        <form>
+                            <Radio
+                                checked={this.state.selectedValue === 'arrive'}
+                                onChange={this.handleChangeRadio}
+                                value="arrive"
+                                name="radio-button-demo"
+                                color="primary"
+                                aria-label="A"
+                            />
+                            <label htmlFor="arrive">Прибытие</label><br />
+                            <Radio
+                                checked={this.state.selectedValue === 'departure'}
+                                onChange={this.handleChangeRadio}
+                                value="departure"
+                                name="radio-button-demo"
+                                color="primary"
+                                aria-label="B"
+                            />
+                            <label htmlFor="departure">Вылет</label><br />
+                            <Switch
+                                checked={this.state.checkedA}
+                                onChange={this.handleChangeSwitch('checkedA')}
+                                value="delayed"
+                                color="primary"
+                            />
+                            <label htmlFor="delayed">Задерживается</label>
+                        </form>
+                    </div>
+                </Grid>
+            </React.Fragment>
+        );
+    }
+}
 
 export default SearchContainer;
