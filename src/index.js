0d456b68f53d2d9b6a69b5080816ec29f72b7d38
AvenMerk <kremnevaaj@gmail.com>
2019-01-19 21:28:50 +0300

    use sync reducers

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import { applyMiddleware, createStore } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 
 import reducer from './reducers'
 
-const middleware = [thunk];
 const store = createStore(
     reducer,
-    applyMiddleware(...middleware)
 );
 
 ReactDOM.render(
