5234f5e6188f07f5898b27ff97b3dbaa359986da
AvenMerk <kremnevaaj@gmail.com>
2019-01-19 22:23:51 +0300

    render flight data

--- src/components/flight.js
+++ src/components/flight.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-const Flight = ({time, city, number, status}) => (
-  <Paper className="aero-flex">
-      <Grid item xs={3}><p className="aero-centred">{city}</p></Grid>
-      <Grid item xs={3}><p className="aero-centred">{time}</p></Grid>
-      <Grid item xs={3}><p className="aero-centred">{number}</p></Grid>
-      <Grid item xs={3}><p className="aero-centred">{status}</p></Grid>
-  </Paper>
-);
+const Flight = ({ data }) => {
+    const {Arrival, Departure, MarketingCarrier} = data;
+    return (
+        <Paper className="aero-flex">
+            <Grid item xs={3}><p className="aero-centred">{Arrival.AirportCode} => {Departure.AirportCode}</p></Grid>
+            <Grid item xs={3}><p className="aero-centred">time</p></Grid>
+            <Grid item xs={3}><p className="aero-centred">{MarketingCarrier.AirlineID} {MarketingCarrier.FlightNumber}</p></Grid>
+            <Grid item xs={3}><p className="aero-centred">{Arrival.TimeStatus.Definition}</p></Grid>
+        </Paper>
+    )
+};
 
 export default Flight;
--- src/containers/flightTable.js
+++ src/containers/flightTable.js
@@ -4,55 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import { connect } from 'react-redux';
 import {fetchFlights} from "../actions";
 
-const flights = [
-    {
-        time: '20:00',
-        city: 'Москва',
-        number: 'AZ202',
-        status: 'В ожидании посадки'
-    }, {
-        time: '22:00',
-        city: 'Рим',
-        number: 'TY205',
-        status: 'Задерживается'
-    }, {
-        time: '19:20',
-        city: 'Воронеж',
-        number: 'AW502',
-        status: 'В полете'
-    }, {
-        time: '02:00',
-        city: 'Севастополь',
-        number: 'AZ202',
-        status: 'В ожидании посадки'
-    }, {
-        time: '12:00',
-        city: 'Лондон',
-        number: 'AZ202',
-        status: 'Задерживается'
-    }, {
-        time: '14:00',
-        city: 'Москва',
-        number: 'AZ202',
-        status: 'В полете'
-    }, {
-        time: '20:30',
-        city: 'Афины',
-        number: 'AZ202',
-        status: 'Посадка закончена'
-    }, {
-        time: '05:00',
-        city: 'Чебоксары',
-        number: 'AZ202',
-        status: 'Отменен'
-    }, {
-        time: '13:00',
-        city: 'Владивосток',
-        number: 'AZ202',
-        status: 'Посадка закончена'
-    },
-];
-
 class FlightTable extends React.Component {
 
     componentDidMount() {
@@ -74,7 +25,7 @@ class FlightTable extends React.Component {
             <Grid item xs={9}>
                 <div className="aero-table-block">
                     <div className="aero-flex aero-add-zero-margin">
-                        <Grid item xs={3}><p className="aero-centred">Город</p></Grid>
+                        <Grid item xs={3}><p className="aero-centred">Код аэропорта</p></Grid>
                         <Grid item xs={3}><p className="aero-centred">Время</p></Grid>
                         <Grid item xs={3}><p className="aero-centred">Номер рейса</p></Grid>
                         <Grid item xs={3}><p className="aero-centred">Статус</p></Grid>
@@ -82,13 +33,9 @@ class FlightTable extends React.Component {
                 </div>
 
                 <div>
-                    {flights.map((flight, index) =>
+                    {this.props.flightData.map((data, index) =>
                         <div key={index}>
-                            <Flight time={flight.time}
-                                    city={flight.city}
-                                    number={flight.number}
-                                    status={flight.status}
-                            />
+                            <Flight data={data} />
                         </div>
                     )}
                 </div>
