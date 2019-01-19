9012c8d1f17dd20f98b77193c4ff2520a271d496
AvenMerk <kremnevaaj@gmail.com>
2019-01-19 13:55:12 +0300

    use DEPARTURES in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { SELECT_MODE } from '../actions';
 import { combineReducers } from 'redux';
+import { DEPARTURES } from '../containers/searchContainer';
 
-const selectedMode = (state = 'departures', action) => {
+const selectedMode = (state = DEPARTURES, action) => {
     switch (action.type) {
         case SELECT_MODE:
             return action.mode;
