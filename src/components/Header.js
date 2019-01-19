88f24fc9f6c2fb1a01bc9a7edbcd48733da855fd
AvenMerk <kremnevaaj@gmail.com>
2019-01-19 15:31:34 +0300

    Edit header h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
     <React.Fragment>
       <header className="aero-header">
-          <h1>Табло рейсов</h1>
+          <h1>Расписание рейсов</h1>
       </header>
     </React.Fragment>
 );
