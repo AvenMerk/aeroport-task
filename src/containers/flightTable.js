acb8e367e4e0bd7b8a04eba9e8f2581b0561aaec
AvenMerk <kremnevaaj@gmail.com>
2019-01-20 17:10:33 +0300

    edit column's name

--- src/containers/flightTable.js
+++ src/containers/flightTable.js
@@ -8,14 +8,12 @@ import Arrival from '../components/arrival'
 class FlightTable extends React.Component {
 
     componentDidMount() {
-        console.log('did mount');
         const { dispatch, selectedMode } = this.props;
         dispatch(fetchFlights(selectedMode))
     }
 
     componentDidUpdate(prevProps) {
         if (prevProps.selectedMode !== this.props.selectedMode) {
-            console.log('did update');
             const { dispatch, selectedMode } = this.props;
             dispatch(fetchFlights(selectedMode))
         }
@@ -27,7 +25,7 @@ class FlightTable extends React.Component {
                 <div className="aero-table-block">
                     <div className="aero-flex aero-add-zero-margin">
                         <Grid item xs={3}><p className="aero-centred">Время</p></Grid>
-                        <Grid item xs={3}><p className="aero-centred">Код аэропорта</p></Grid>
+                        <Grid item xs={3}><p className="aero-centred">Город</p></Grid>
                         <Grid item xs={3}><p className="aero-centred">Номер рейса</p></Grid>
                         <Grid item xs={3}><p className="aero-centred">Статус</p></Grid>
                     </div>
