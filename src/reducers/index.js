652ac8b25e9594a8d4ad9b7085bb91cf871c9da8
AvenMerk <kremnevaaj@gmail.com>
2019-01-20 17:08:37 +0300

    Add selectedDelayed reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,21 @@
-import { SELECT_MODE, FETCH_FLIGHTS } from '../actions';
+import { SELECT_MODE, FETCH_FLIGHTS, SELECT_DELAYED } from '../actions';
 import { combineReducers } from 'redux';
 import { DEPARTURES } from '../containers/searchContainer';
+
 
 const selectedMode = (state = DEPARTURES, action) => {
     switch (action.type) {
         case SELECT_MODE:
             return action.mode;
+        default:
+            return state;
+    }
+};
+
+const selectedDelayed = (state = false, action) => {
+    switch (action.type) {
+        case SELECT_DELAYED:
+            return action.delayed;
         default:
             return state;
     }
@@ -26,5 +36,6 @@ const flightsForMode = (state = {flightData: []}, action) => {
 export default combineReducers({
     selectedMode,
     flightsForMode,
+    selectedDelayed,
 })
 
