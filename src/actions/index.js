34ffd1d48f64222bbdd848f02d699cb673a7afa3
AvenMerk <kremnevaaj@gmail.com>
2019-01-20 17:05:25 +0300

    Add selectDelayed action

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,12 +4,20 @@ import departures from '../data/departures.json';
 
 export const SELECT_MODE = 'SELECT_MODE';
 export const FETCH_FLIGHTS = 'FETCH_FLIGHTS';
+export const SELECT_DELAYED = 'SELECT_DELAYED';
 
 export const selectMode = mode => {
     return {
         type: SELECT_MODE,
         mode
     }
+};
+
+export const selectDelayed = delayed => {
+  return {
+      type: SELECT_DELAYED,
+      delayed
+  }
 };
 
 export const fetchFlights = mode => {
