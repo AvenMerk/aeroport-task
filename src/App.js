0e7c9b2ed6e62ec5818afed3e840ccea3d0efd8f
AvenMerk <kremnevaaj@gmail.com>
2019-01-20 14:24:03 +0300

    refactor file name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/App.css';
-import Header from './components/Header';
+import Header from './components/header';
 import FlightTable from './containers/flightTable';
 import SearchContainer from './containers/searchContainer';
 
