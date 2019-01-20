3588ea792e0cb8e718be84bf57acaaf9012a4448
AvenMerk <kremnevaaj@gmail.com>
2019-01-20 14:20:30 +0300

    format date in flight schedule

--- src/components/flight.js
+++ src/components/flight.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import dateFormat from 'dateformat';
 
 const Flight = ({ data }) => {
     const {Arrival, Departure, MarketingCarrier} = data;
+    const date = dateFormat(Departure.ScheduledTimeLocal.DateTime, 'HH:MM');
     return (
         <Paper className="aero-flex">
-            <Grid item xs={3}><p className="aero-centred">{Arrival.AirportCode} => {Departure.AirportCode}</p></Grid>
-            <Grid item xs={3}><p className="aero-centred">time</p></Grid>
+            <Grid item xs={3}><p className="aero-centred">{Departure.AirportCode} => {Arrival.AirportCode}</p></Grid>
+            <Grid item xs={3}><p className="aero-centred">{date}</p></Grid>
             <Grid item xs={3}><p className="aero-centred">{MarketingCarrier.AirlineID} {MarketingCarrier.FlightNumber}</p></Grid>
             <Grid item xs={3}><p className="aero-centred">{Arrival.TimeStatus.Definition}</p></Grid>
         </Paper>
